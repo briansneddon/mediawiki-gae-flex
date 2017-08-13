2cefda1a62d1c626c2e52dc0de22b1818e96cbf2
Brian Sneddon <bsneddon@gc1.com>
2017-08-12 21:51:37 -0500

    Support socket paths/filenames containing colons GAE Flex access to Cloud SQL is via the cloud sql proxy which exposes a socket in /cloudsql which is named using the GCP project, region, and db name separated by colons.

--- includes/libs/rdbms/database/DatabaseMysqli.php
+++ includes/libs/rdbms/database/DatabaseMysqli.php
@@ -74,10 +74,10 @@ class DatabaseMysqli extends DatabaseMysqlBase {
 			if ( $hostAndPort[1] ) {
 				$port = $hostAndPort[1];
 			}
-		} elseif ( substr_count( $realServer, ':' ) == 1 ) {
+		} elseif ( substr_count( $realServer, ':' ) >= 1 ) {
 			// If we have a colon and something that's not a port number
 			// inside the hostname, assume it's the socket location
-			$hostAndSocket = explode( ':', $realServer );
+			$hostAndSocket = explode( ':', $realServer, 2 );
 			$realServer = $hostAndSocket[0];
 			$socket = $hostAndSocket[1];
 		}
